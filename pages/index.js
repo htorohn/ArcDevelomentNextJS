119f691e65ed991bb9af1dfa7559262f37b49012
htoro <htorohn@gmail.com>
2020-10-07 18:23:32 -0600

    testing environment variables 2

--- pages/index.js
+++ pages/index.js
@@ -149,7 +149,7 @@ const LandingPage = (props) => {
 				<title key='title'>
 					Custom Software, Mobile Apps, and Websites | Arc Development
 				</title>
-				<meta name='hello' content={process.env.DB_HOST} />
+				<meta name='hello-world' content={process.env.DB_HOST} />
 				<meta
 					name='description'
 					key='description'
